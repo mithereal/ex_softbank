bfca78f819c78a66da63231c41701adf4d9682b8
Jason Clark <mithereal@gmail.com>
2018-08-10 15:18:40 -0700

    .

--- mix.exs
+++ mix.exs
@@ -30,6 +30,7 @@ defmodule SoftBank.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ecto, "~> 2.1"}
     ]
   end
   
