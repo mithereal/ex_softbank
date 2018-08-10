2f11833fee8efb0d67cfbf588f751a10cd3c1d62
Jason Clark <mithereal@gmail.com>
2018-08-10 15:10:01 -0700

    .

--- mix.exs
+++ mix.exs
@@ -7,8 +7,8 @@ defmodule Softbank.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
-        build_embedded: Mix.env == :prod,
+      deps: deps(),
+      build_embedded: Mix.env == :prod,
       description: description(),
       package: package(),
       elixir: "~> 1.0",
