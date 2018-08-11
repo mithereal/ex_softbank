df2a46edda20d00c9f17578c301be76968f627be
Jason Clark <mithereal@gmail.com>
2018-08-11 08:49:26 -0700

    .

--- config/config.exs
+++ config/config.exs
@@ -32,37 +32,6 @@ use Mix.Config
 config :soft_bank,
        soft_bank: [SoftBank.Repo]
        
-       
-config :soft_bank, SoftBank.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "",
-  database: "public",
-  hostname: "localhost",
-  pool_size: 10
-
-config :soft_bank, app_mode: "test"
-
-config :soft_bank,
-       default_currency: :USD,
-       separator: ".",
-       delimiter: ",",
-       symbol: false,
-       symbol_on_right: false,
-       symbol_space: false,
-       fractional_unit: false
-
-config :soft_bank,
-       pool_size: 10,
-       pool_max_overflow: 1
-
-config :soft_bank,
-       #source: SoftBank.Currency.Conversion.Source.Fixer,
-       source: SoftBank.Currency.Conversion.Source.Test,
-       #source_api_key: "FIXER_ACCESS_KEY",
-       # defaults to http since free access key only supports http
-
-       source_protocol: "https",
-       refresh_interval: 86_400_000
+ 
 
 import_config "#{Mix.env()}.exs"
