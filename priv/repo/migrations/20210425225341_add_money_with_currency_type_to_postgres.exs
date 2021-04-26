5c2635230927d0aeb46fec7165978689974b384b
mithereal <mithereal@gmail.com>
2021-04-26 19:17:06 +0000

    add custom currency auto-reload

---
+++ priv/repo/migrations/20210425225341_add_money_with_currency_type_to_postgres.exs
@@ -0,0 +1,13 @@
+defmodule SoftBank.Repo.Migrations.AddMoneyWithCurrencyTypeToPostgres do
+  use Ecto.Migration
+
+  def up do
+    execute("CREATE TYPE public.money_with_currency AS (currency_code char(3), amount numeric);")
+    execute("CREATE TYPE public.currency AS (currency_code char(3));")
+  end
+
+  def down do
+    execute("DROP TYPE public.money_with_currency;")
+    execute("DROP TYPE public.currency;")
+  end
+end
