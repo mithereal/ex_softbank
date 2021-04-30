aa234d7092587aecb4d56199953285f392c2e70f
mithereal <mithereal@gmail.com>
2021-04-30 23:03:02 +0000

    .

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule SoftBank.MixProject do
     [
       app: :soft_bank,
       version: @version,
-      elixir: "~> 1.10",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
