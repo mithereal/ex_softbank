47608dd0b622ba8b1f8050cf57dc31a32f1c90fd
mithereal <mithereal@gmail.com>
2024-03-08 20:23:01 -0700

    relax nanoid requirement

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule SoftBank.MixProject do
       {:ex_money, ">= 0.0.0"},
       {:ex_money_sql, "~> 1.0"},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false},
-      {:nanoid, "~> 2.0"},
+      {:nanoid, "> 2.0.0"},
       {:inch_ex, ">= 0.0.0", only: :docs},
       {:faker, "~> 0.17", only: :test},
       {:ex_machina, ">= 0.0.0", only: :test}
